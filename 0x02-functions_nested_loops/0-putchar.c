d87fec790fe0e8bef03e6db2f0fb07ea4b84cdb4
Sarah <sarahanwarisamel@gmail.com>
2023-08-16 09:41:59 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - desc
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0, j;
+
+	while (i < 1)
+	{
+		j = 0;
+		while (j < 1)
+		{
+			printf("_putchar ");
+			j++;
+		}
+
+		printf("\n");
+		i++;
+	}
+
+	return (0);
+}
