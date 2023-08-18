91bc68f799e3f3901efc6d0b63757272fbed7d50
Sarah <sarahanwarisamel@gmail.com>
2023-08-18 06:03:36 -0700

    done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks
+ * @x: the num
+ * Return: 1 for
+ */
+
+int _isupper(int x)
+{
+
+	if (x >= 65 && x <= 98)
+	{
+		return (1);
+	}
+	return (0);
+}
+
